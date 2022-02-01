819f0619d5bc44b2661c4c7f2cdc5098b47c4ca9
EmilyCV <emily_cvasconcelos@hotmail.com>
2022-02-01 19:26:10 -0300

    feat: new column in user table and create table cateto

--- persistence/database/database_objects.py
+++ persistence/database/database_objects.py
@@ -1,21 +1,38 @@
 import sqlite3
 
 # Conectando
-conn = sqlite3.connect('database.db',check_same_thread=False)
+conn = sqlite3.connect('database.db', check_same_thread=False)
 
 # Definindo um cursor
 cursor = conn.cursor()
 
+
 def create_db():
     cursor.execute(f"""
     CREATE TABLE user (
-            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-            nome TEXT NOT NULL);   
+    email TEXT NOT NULL
+               PRIMARY KEY,
+    nome  TEXT NOT NULL
+);
+CREATE TABLE catetos (
+    id              INTEGER NOT NULL
+                            PRIMARY KEY AUTOINCREMENT,
+    catetoOposto    NUMERIC NOT NULL,
+    catetoAdjacente NUMERIC NOT NULL,
+    resultado       NUMERIC NOT NULL,
+    email           STRING  NOT NULL,
+    FOREIGN KEY (
+        email
+    )
+    REFERENCES user (email) 
+);  
     """)
 
-def create_user(name):
+
+def create_user(email, name):
     with sqlite3.connect('database.db') as con:
-        cursor.execute("INSERT INTO user (nome) VALUES (?)",(name,))
+        cursor.execute(
+            "INSERT INTO user (email,nome) VALUES (?,?)", (email, name))
         cursor.execute('COMMIT')
         msg = "Done"
         conn.close()
